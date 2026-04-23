36a27664fb822e605a723dae7bfb823d814db57c
TSchababerle <tschabab@gmail.com>
2026-04-23 10:10:12 -0500

    Update Figure2E.r

--- Figures/Figure2E.r
+++ Figures/Figure2E.r
@@ -5,29 +5,31 @@ library(ggplot2)
 library(tidyverse)
 library(viridis)
 
+#Figure 2E: four cropped xenium images colored by pseudotime bins.
+
 #crop for morphology transition zoomed
-crop1 <- Crop(mesoTumor[["fov"]], y = c(8600, 9000), x = c(5300, 5800), coords = "plot")
+crop1 <- Crop(meso.WS.Tumor[["fov"]], y = c(8600, 9000), x = c(5300, 5800), coords = "plot")
 #apply cropped coords
-mesoTumor[["zoom1"]] <- crop1
-DefaultBoundary(mesoTumor[["zoom1"]]) <- "segmentation"
+meso.WS.Tumor[["zoom1"]] <- crop1
+DefaultBoundary(meso.WS.Tumor[["zoom1"]]) <- "segmentation"
 
 #crop for morphology transition zoomed epithelioid
-crop2 <- Crop(mesoTumor[["fov"]], y = c(8600, 8800), x = c(5300, 5600), coords = "plot")
+crop2 <- Crop(meso.WS.Tumor[["fov"]], y = c(8600, 8800), x = c(5300, 5600), coords = "plot")
 #apply cropped coords
-mesoTumor[["zoom2"]] <- crop2
-DefaultBoundary(mesoTumor[["zoom2"]]) <- "segmentation"
+meso.WS.Tumor[["zoom2"]] <- crop2
+DefaultBoundary(meso.WS.Tumor[["zoom2"]]) <- "segmentation"
 
 #crop for morphology transition zoomed transition 
-crop3 <- Crop(mesoTumor[["fov"]], y = c(8700, 8900), x = c(5400, 5700), coords = "plot")
+crop3 <- Crop(meso.WS.Tumor[["fov"]], y = c(8700, 8900), x = c(5400, 5700), coords = "plot")
 #apply cropped coords
-mesoTumor[["zoom3"]] <- crop3
-DefaultBoundary(mesoTumor[["zoom3"]]) <- "segmentation"
+meso.WS.Tumor[["zoom3"]] <- crop3
+DefaultBoundary(meso.WS.Tumor[["zoom3"]]) <- "segmentation"
 
 #crop for morphology transition zoomed sarcomatoid
-crop4 <- Crop(mesoTumor[["fov"]], y = c(8800, 9000), x = c(5500, 5800), coords = "plot")
+crop4 <- Crop(meso.WS.Tumor[["fov"]], y = c(8800, 9000), x = c(5500, 5800), coords = "plot")
 #apply cropped coords
-mesoTumor[["zoom4"]] <- crop4
-DefaultBoundary(mesoTumor[["zoom4"]]) <- "segmentation"
+meso.WS.Tumor[["zoom4"]] <- crop4
+DefaultBoundary(meso.WS.Tumor[["zoom4"]]) <- "segmentation"
 
 #plot colors
 plasma.grad <- plasma(40)
@@ -42,11 +44,11 @@ tumor.colors <- c(
   "Epithelioid proliferating tumor" = "#ff8fb3")
   
 #plot with correct segmentation boundaries
-p1 <- ImageDimPlot(mesoTumor, fov = "zoom1", group.by = "refined_tumor_cluster", cols = tumor.colors, border.size = 0, na.value = "grey80") +
+p1 <- ImageDimPlot(meso.WS.Tumor, fov = "zoom1", group.by = "refined_tumor_cluster", cols = tumor.colors, border.size = 0, na.value = "grey80") +
   theme(panel.grid = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
 
 #plot with incorrect segmentation boundaries
-p2 <- ImageFeaturePlot(mesoTumor, fov = "zoom1", features = "pseudotime", cols = plasma.grad, dark.background = TRUE, 
+p2 <- ImageFeaturePlot(meso.WS.Tumor, fov = "zoom1", features = "pseudotime", cols = plasma.grad, dark.background = TRUE, 
                        border.size = 0.15, boundaries = "segmentations", border.color = "black") + 
   theme(panel.grid = element_blank(), axis.ticks = element_blank(), axis.text = element_blank()) + 
   theme(legend.position = "none") + labs(title = NULL)
@@ -61,13 +63,13 @@ p2[[1]]$data <- p2[[1]]$data %>%
 p2
 
 #plot crop2
-ImageDimPlot(mesoTumor, fov = "zoom2", group.by = "bin_Epithelioid", cols = c("gray80", purple.hue), border.size = 0.1,
+ImageDimPlot(meso.WS.Tumor, fov = "zoom2", group.by = "bin_Epithelioid", cols = c("gray80", purple.hue), border.size = 0.1,
   border.color = "white", axes = FALSE, na.value = "grey80") + theme(legend.position = "none") + labs(title = NULL)
   
  #plot crop3
-ImageDimPlot(mesoTumor, fov = "zoom3", group.by = "bin_Transition", cols = c("gray80", pink.hue), border.size = 0.1,
+ImageDimPlot(meso.WS.Tumor, fov = "zoom3", group.by = "bin_Transition", cols = c("gray80", pink.hue), border.size = 0.1,
   border.color = "white", axes = FALSE, na.value = "grey80") + theme(legend.position = "none") + labs(title = NULL)
   
  #plot crop4
-ImageDimPlot(mesoTumor, fov = "zoom4", group.by = "bin_Sarcomatoid", cols = c("gray80", yellow.hue), border.size = 0.1,
+ImageDimPlot(meso.WS.Tumor, fov = "zoom4", group.by = "bin_Sarcomatoid", cols = c("gray80", yellow.hue), border.size = 0.1,
   border.color = "white", axes = FALSE, na.value = "grey80") + theme(legend.position = "none") + labs(title = NULL)
