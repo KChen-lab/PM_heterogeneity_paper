3846025d4475ce51f0f4c47770cf2fbadba89236
TSchababerle <tschabab@gmail.com>
2026-04-23 10:07:41 -0500

    Update Figure2D.r

--- Figures/Figure2D.r
+++ Figures/Figure2D.r
@@ -5,15 +5,17 @@ library(ggplot2)
 library(tidyverse)
 library(viridis)
 
+#figure 2D: xenium image colored by pseudotime.
+
 #color gradient
 plasma_grad <- plasma(40)
 
 #plot with correct segmentation boundaries
-p1 <- ImageDimPlot(meso, fov = "fov", group.by = "refined_tumor_cluster", cols = cell.colors, border.size = 0.1) +
+p1 <- ImageDimPlot(meso.WS.Tumor, fov = "fov", group.by = "refined_tumor_cluster", cols = cell.colors, border.size = 0.1) +
   theme(panel.grid = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
 
 #plot with incorrect segmentation boundaries
-p2 <- ImageFeaturePlot(meso, fov = "fov", features = "pseudotime", cols = plasma_grad, dark.background = TRUE, 
+p2 <- ImageFeaturePlot(meso.WS.Tumor, fov = "fov", features = "pseudotime", cols = plasma_grad, dark.background = TRUE, 
                  border.size = 0, boundaries = "segmentations") +
   theme(panel.grid = element_blank(), axis.ticks = element_blank(), axis.text = element_blank())
 
@@ -23,3 +25,5 @@ p2[[1]]$data <- p2[[1]]$data %>%
             by = "cell") %>%
   mutate(x = x_correct, y = y_correct,
          x_correct = NULL, y_correct = NULL)
+
+p2
