7266ab1c3a558ab88a9485ae67811bdd85948ce5
TSchababerle <tschabab@gmail.com>
2026-04-23 09:53:40 -0500

    Update Figure1E.r

--- Figures/Figure1E.r
+++ Figures/Figure1E.r
@@ -4,10 +4,8 @@ library(Seurat)
 library(ggplot2)
 library(tidyverse)
 
-##panel E
-#create tumor subcluster
-meso.WS.Tumor <- subset(meso.WS, idents = c("Epithelioid tumor", "Epithelioid proliferating tumor",
-                                     "Sarcomatoid proliferating tumor", "Sarcomatoid tumor"))
+##panel E: Dot plot showing marker gene expression for the four tumor states
+#using the tumor cell only whole slide surgical resection object.
 
 #order of cell types on dotplot
 cell.type.order <- c("Sarcomatoid tumor", "Sarcomatoid proliferating tumor",
@@ -21,12 +19,8 @@ Idents(meso.WS.Tumor) <- meso.WS.Tumor$celltype_tumor
 DotPlot(meso.WS.Tumor, features = c("CDH1", "CDH3", "MSLN", "CALB2", "WT1",
                                "TOP2A", "MKI67", "AURKA", "SNAI2", "TWIST1", "TWIST2", "MYBL2",
                                "THBS2", "COL5A2", "LTBP2", "COL5A1", "AXL", "GAS6", "ITGB4"), 
-        group.by="celltype") + RotatedAxis() + 
-  scale_color_gradient(low = "lightgrey", high = "#1E90FF") +
-  ggplot2::theme(
-    axis.title.x = ggplot2::element_text(size = 8),
-    axis.title.y = ggplot2::element_text(size = 8),
-    axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1, size = 8),
-    axis.text.y = ggplot2::element_text(size = 8)) +
+        group.by="celltype") + RotatedAxis() + scale_color_gradient(low = "lightgrey", high = "#1E90FF") +
+  ggplot2::theme( axis.title.x = ggplot2::element_text(size = 8), axis.title.y = ggplot2::element_text(size = 8),
+    axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1, size = 8), axis.text.y = ggplot2::element_text(size = 8)) +
   ggplot2::scale_y_discrete(labels = function(x) str_wrap(x, width = 16)) + 
   theme(legend.text = element_text(size = 8)) + theme(legend.title = element_text(size = 8))
