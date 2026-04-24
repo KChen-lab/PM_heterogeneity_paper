96c770c57f7f0dddf3a491338b9f14f7024b6173
TSchababerle <tschabab@gmail.com>
2026-04-24 10:18:43 -0500

    Update Figure5D.r

--- Figures/Figure5D.r
+++ Figures/Figure5D.r
@@ -4,5 +4,7 @@ library(cellchat2)
 library(ggplot2)
 library(tidyverse)
 
+#Figure 5d: cellchat ligand-receptor pair plot for interstitial macrophages (6) and tumor cells (3).
+
 #plot ligand-receptor dot plot
 netVisual_bubble(cellchat, sources.use = c(6), targets.use = c(3,6), comparison = c(1,2), angle.x = 45)
