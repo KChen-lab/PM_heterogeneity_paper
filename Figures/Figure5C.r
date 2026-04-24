519e82a33e443bb3c1d85cf23db101f9163563e8
TSchababerle <tschabab@gmail.com>
2026-04-24 10:18:07 -0500

    Update Figure5C.r

--- Figures/Figure5C.r
+++ Figures/Figure5C.r
@@ -4,5 +4,7 @@ library(cellchat2)
 library(ggplot2)
 library(tidyverse)
 
+#Figure 5C: cellchat ligand-receptor pair plot for tumor cells (3) and CD8+ T cells (7).
+
 #plot ligand-receptor dot plot
-netVisual_bubble(cellchat, sources.use = c(6), targets.use = c(3,6), comparison = c(1,2), angle.x = 45)
+netVisual_bubble(cellchat, sources.use = c(3), targets.use = c(3,7), comparison = c(1,2), angle.x = 45)
