f598db11f9c8e59396ff50d6a060dd6d8c9ff011
TSchababerle <tschabab@gmail.com>
2026-04-24 10:15:50 -0500

    Update Figure5B.r

--- Figures/Figure5B.r
+++ Figures/Figure5B.r
@@ -4,5 +4,7 @@ library(cellchat2)
 library(ggplot2)
 library(tidyverse)
 
+#Figure5B: cellchat comparative interaction weight bar plot for xenium meso TMA data.
+
 #plot bar plot
 compareInteractions(cellchat, show.legend = FALSE, group = c(1,2), measure = "weight")
