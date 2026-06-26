9539d2e4df8186164983d0886913f2c62fb6bdcd
TSchababerle <tschabab@gmail.com>
2026-06-26 09:50:46 -0500

    Update whole_slide_visium_object.r

--- Seurat_objects/whole_slide_visium_object.r
+++ Seurat_objects/whole_slide_visium_object.r
@@ -6,3 +6,16 @@ library(tidyverse)
 
 #create seurat object from visium whole slide (WS)
 vis.WS <- LoadData ("/path to data/",)
+
+#subset spatial features to include >200 features per spot
+vis.WS <- vis.WS[, vis.WS$n_Feature_Spatial > 200]
+
+#normalize the data 
+
+
+#find cell markers per cluster
+Idents(meso) <- "seurat_clusters"
+markers <- FindAllMarkers(meso, only.pos = TRUE, min.pct = 0.1, logfc.threshold = 0.25)
+
+#save object
+saveRDS(vis.WS, file = "MESO_Whole_Slide_visium.rds")
