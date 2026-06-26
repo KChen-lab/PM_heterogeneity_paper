230f6c616afa73989ff6134665e191d52efcdce4
TSchababerle <tschabab@gmail.com>
2026-06-26 10:15:53 -0500

    Update whole_slide_visium_object.r

--- Seurat_objects/whole_slide_visium_object.r
+++ Seurat_objects/whole_slide_visium_object.r
@@ -10,12 +10,21 @@ vis.WS <- LoadData ("/path to data/",)
 #subset spatial features to include >200 features per spot
 vis.WS <- vis.WS[, vis.WS$n_Feature_Spatial > 200]
 
-#normalize the data 
+#normalization, dimensionalty reduction, and clustering
+vis.WS <- vis.WS %>%
+  NormalizeData() %>%
+  FindVariableFeatures() %>%
+  ScaleData()
 
+vis.WS <- vis.WS %>%
+  RunPCA(npcs = 30) %>%
+  FindNeighbors(dims = 1:30) %>%
+  FindClusters(resoltuion = 0.8) %>%
+  RunUMAP(dims = 1:30)
 
 #find cell markers per cluster
-Idents(meso) <- "seurat_clusters"
-markers <- FindAllMarkers(meso, only.pos = TRUE, min.pct = 0.1, logfc.threshold = 0.25)
+Idents(vis.WS) <- "seurat_clusters"
+markers <- FindAllMarkers(vis.WS, only.pos = TRUE, min.pct = 0.1, logfc.threshold = 0.25)
 
 #save object
 saveRDS(vis.WS, file = "MESO_Whole_Slide_visium.rds")
