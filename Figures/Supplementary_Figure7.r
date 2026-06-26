a4c9d4b67188eb95a705f88346a0246094e5670d
TSchababerle <tschabab@gmail.com>
2026-06-26 09:30:56 -0500

    Update Supplementary_Figure7.r

--- Figures/Supplementary_Figure7.r
+++ Figures/Supplementary_Figure7.r
@@ -4,28 +4,49 @@ library(Seurat)
 library(ggplot2)
 library(tidyverse)
 
-#colors
-patient.colors <- c(
-  "1" = "#ff3155", "2" = "#ffaf42", "3" = "#ffed5e", "4" = "#49f770", "5" = "#2daefd", "6" = "#9d4dff")
+#cell cycle genes 
+s.genes.xen <- c("MCM5", "PCNA", "TYMS", "FEN1", "MCM7", "MCM4", "MCM6", "RRM1", "UNG", "UHRF1",
+                 "HELLS", "MSH2", "RAD51", "RRM2", "EXO1", "BLM", "CASP8AP2")
+g2m.genes.xen <- c("HMGB2", "CDK1", "NUSAP1", "UBE2C", "BIRC5", "TPX2", "TOP2A", "CKS2", "MKI67",
+                   "TMPO", "CENPF", "PIMREG", "SMC4", "CCNB2", "CKAP2", "AURKB", "AURKA", "BUB1",
+                   "KIF11", "ANP32E", "CDCA3", "CDC20", "TTK", "CDC25C", "KIF2C", "RANGAP1",
+                   "CDCA2", "HMMR", "PSRC1", "CKAP5", "CTCF", "NEK2", "CBX5", "CENPA")
 
-#arrow placement on umap
-umap <- Embeddings(meso, "umap")
-xmin <- min(umap[,1])
-ymin <- min(umap[,2])
+#transition signature
+trans.sig <- c("SNAI2", "TWIST1", "TWIST2", "MYBL2")
 
-#umap for patient ID
-DimPlot(meso, group.by = "patient_number", cols = patient.colors, label = FALSE, label.size = 4, raster = FALSE) +
-  guides(color = guide_legend(ncol = 1, override.aes = list(size = 5))) +  theme_void() +
-  annotate("segment",
-           x = xmin, xend = xmin + 2,
-           y = ymin, yend = ymin,
-           arrow = arrow(length = unit(0.2,"cm"))) +
-  annotate("segment",
-           x = xmin, xend = xmin,
-           y = ymin, yend = ymin + 2,
-           arrow = arrow(length = unit(0.2,"cm"))) +
-  annotate("text", x = xmin + 2, y = ymin - 0.5, label = "UMAP1") +
-  annotate("text", x = xmin - 0.5, y = ymin + 2, label = "UMAP2", angle = 90) + theme(
-    legend.title = element_blank(),
-    plot.title = element_text(hjust = 0.5)) + ggtitle("Cell Type Annotation")
+#assign cell cycle scores
+meso <- CellCycleScoring(meso, s.features = s.genes.xen, g2m.features = g2m.genes.xen, set.ident = TRUE)
 
+#score transition signaturr with module score
+meso.WS <- AddModuleScore(meso.WS, features=list(trans.sig), name= "Transition_signature", ctrl = 50)
+
+#statistics
+g2m.cor <- cor.test(meso.WS$Transition_signature1, meso.WS$G2M.Score, method = "spearman", exact = FALSE)
+g2m.cor$estimate
+g2m.cor$p.value
+
+g2m.rho <- round(unname(g2m.cor$estimate), 3)
+g2m.p.label <- ifelse(g2m.cor$p.value < 2.2e-16,
+  "p < 2.2e-16", paste0("p = ", signif(g2m.cor$p.value, 3)))
+g2m.label <- paste0("rho = ", rho, "\n", g2m.p.label)
+
+s.cor <- cor.test(meso.WS$Transition_signature1, meso$S.Score, method = "spearman")
+s.cor$estimate
+s.cor$p.value
+
+s.rho <- round(unname(s.cor$estimate), 3)
+s.p.label <- ifelse(s.cor$p.value < 2.2e-16,
+                      "p < 2.2e-16", paste0("p = ", signif(s.cor$p.value, 3)))
+s.label <- paste0("rho = ", rho, "\n", s.p.label)
+
+
+#cell cycle correlated with transition
+g1 <- FeatureScatter(meso.WS, feature1 = "Transition_signature1", feature2 = "G2M.Score", group.by = "Phase") +
+  annotate("text", x = Inf, y = Inf, hjust = 1.1, vjust = 1.5,
+           label = g2m.label)
+
+g2 <- FeatureScatter(meso.WS, feature1 = "Transition_signature1", feature2 = "S.Score", group.by = "Phase") +
+  annotate("text", x = Inf, y = Inf, hjust = 1.1, vjust = 1.5,
+           label = s.label)
+g1 + g2
